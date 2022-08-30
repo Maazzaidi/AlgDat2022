bfd7f844eaefac7eea45e652bb578bf6e8a0a7d7
Maazzaidi <s364704@oslomet.no>
2022-08-30 11:12:32 +0200

    Sceleton code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Teller til ti");
-        for (int i = 1; i <= 10; ++i) {
+        for (int i = 1; i  <= 10; ++i) {
             System.out.println(i);
             }
         System.out.println("Petra teller til tyve");
